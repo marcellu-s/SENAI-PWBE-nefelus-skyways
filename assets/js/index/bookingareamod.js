82c5adf3191fdcbc10ff73b3c9be58c1d397a99c
Marcelo Francisco Costa <115178042+marcellu-s@users.noreply.github.com>
2023-05-05 00:25:20 -0300

    att JS index

--- assets/js/index/bookingareamod.js
+++ assets/js/index/bookingareamod.js
@@ -1,9 +1,12 @@
 const formBookingArea = document.querySelector('.booking-area form');
 
 function clickFunction() {
-    window.location.href = '';
+    window.location.href = 'pages/reservar_passagem/reservar_passagem.html';
 }
 
+if (window.screen.width < 668) {
+    formBookingArea.addEventListener('click', clickFunction);
+}
 
 window.addEventListener('resize', () => {
     if (window.screen.width < 668) {
