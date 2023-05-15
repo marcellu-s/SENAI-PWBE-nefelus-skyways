af22786a67fe077836073a9491b4cb59517632f0
Marcelo Francisco Costa <115178042+marcellu-s@users.noreply.github.com>
2023-05-15 09:19:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -154,7 +154,7 @@ date_default_timezone_set('America/Sao_Paulo');
         <!-- BOOKING-AREA -->
 
         <section class="booking-area">
-            <form action="./selecionar_voo_ida.php" method="post">
+            <form action="./pages/reservar_passagem/selecionar_voo_ida.php" method="post">
                 <div class="input-control mobile-hide input-trecho">
                     <div class="dropdown">
                         <button class="trigger-button" type="button">
